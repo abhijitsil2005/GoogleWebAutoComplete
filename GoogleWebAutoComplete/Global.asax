b8607d3d4c51ed770b7b7e84cf2421c92d3bdcab
Sil, Abhijit <abhijit.sil2005@gmail.com>
2017-10-02 23:40:09 -0400

    Add project files.

---
+++ GoogleWebAutoComplete/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GoogleWebAutoComplete.MvcApplication" Language="C#" %>
---
+++ GoogleWebAutoComplete/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GoogleWebAutoComplete.Startup))]
+namespace GoogleWebAutoComplete
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
